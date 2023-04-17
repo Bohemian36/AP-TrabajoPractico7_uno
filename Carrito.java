dc0cbb56237b89d50dd154da82d22cca4445e376
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-04-17 04:04:20 -0300

    Update Carrito.java

--- Carrito.java
+++ Carrito.java
@@ -1,14 +1,14 @@
 package pack1;
 
-public class Carrito {
+     public class Carrito {
     	private int num;
-	    private Pers cliente;
-	    private double montoTotal;
+	private Pers cliente;
+	private double montoTotal;
 	
-	  public Carrito(Pers cli, int num) {
-	    	this.num = num;
-		    cliente = cli;
-		    montoTotal = 0;
+          public Carrito(Pers cli, int num) {
+	    	 this.num = num;
+		 cliente = cli;
+		 montoTotal = 0;
 	}
 	  public int Num() {
 	    	return num;
@@ -16,7 +16,7 @@ public class Carrito {
 	  public double MontoTotal() {
 		    return montoTotal;
 	}
-  	public void sumarMonto(ItemCarrito subTotal) {
+  	  public void sumarMonto(ItemCarrito subTotal) {
 	    	montoTotal = montoTotal + subTotal.SubTotal();
 	}
 }
